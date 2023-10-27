37266540ddef8b3125592e128d5db5d08bc4b1c9
ManuelF11076 <mfuentesr14@miumg.edu.gt>
2023-10-26 21:20:35 -0600

    Funcion Principal
    esta funcion es la encargada d resolver el problema por el metodo de transporte

--- Librerias y main.py
+++ Librerias y main.py
@@ -3,6 +3,7 @@ from tkinter import PhotoImage  # Importa PhotoImage para manejar imágenes
 from ortools.linear_solver import pywraplp
 import os
 
+
 if __name__ == "__main__":
     root = tk.Tk()
     app = TransportProblemSolverGUI(root)
