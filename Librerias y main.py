9b2f1e5a70a24b6a26149f24d9d2c75e668fcff9
MynorCeron <149128270+MynorCeron@users.noreply.github.com>
2023-10-26 21:27:10 -0600

    Funcion txt

--- Librerias y main.py
+++
@@ -1,9 +0,0 @@
-import tkinter as tk
-from tkinter import PhotoImage  # Importa PhotoImage para manejar imágenes
-from ortools.linear_solver import pywraplp
-import os
-
-if __name__ == "__main__":
-    root = tk.Tk()
-    app = TransportProblemSolverGUI(root)
-    root.mainloop()
