ec3ac5e808c742b78d2231901b9376d56844f5c6
Rúdolf Reyes <148642992+rudolfreyes@users.noreply.github.com>
2023-10-26 21:50:14 -0600

    Update Botones_Procedimiento.py

--- Botones_Procedimiento.py
+++ Botones_Procedimiento.py
@@ -1,16 +1,16 @@
- Etiqueta para mostrar los resultados en la interfaz
+# Etiqueta para mostrar los resultados en la interfaz
        self.result_label = tk.Label(root, text="", font=("Berlin Sans FB", 12), bg="#F0F8FF")
        self.result_label.grid(row=len(self.labels) + len(self.cost_labels) + 4, column=0, columnspan=6, padx=10, pady=10, sticky='w')
 
        Botón para resolver el problema y mostrar la solución en la interfaz
-       solve_button = tk.Button(root, text="Resolver", command=self.solve_and_display, font=("Berlin Sans FB", 14), bg="green", fg="white")
-       solve_button.grid(row=len(self.labels) + len(self.cost_labels) + 2, column=0, padx=10, pady=10, sticky='w')
-        Botón para limpiar las entradas
-       clear_button = tk.Button(root, text="Limpiar", command=self.clear_entries, font=("Berlin Sans FB", 14), bg="orange")
-       clear_button.grid(row=len(self.labels) + len(self.cost_labels) + 2, column=1, padx=10, pady=10, sticky='w')
+        solve_button = tk.Button(root, text="Resolver", command=self.solve_and_display, font=("Berlin Sans FB", 14), bg="green", fg="white")
+        solve_button.grid(row=len(self.labels) + len(self.cost_labels) + 2, column=0, padx=10, pady=10, sticky='w')
+       Botón para limpiar las entradas
+        clear_button = tk.Button(root, text="Limpiar", command=self.clear_entries, font=("Berlin Sans FB", 14), bg="orange")
+        clear_button.grid(row=len(self.labels) + len(self.cost_labels) + 2, column=1, padx=10, pady=10, sticky='w')
        Botón para guardar resultados en un archivo
-       save_button = tk.Button(root, text="Guardar", command=self.save_to_file, font=("Berlin Sans FB", 14), bg="blue", fg="white")
-       save_button.grid(row=len(self.labels) + len(self.cost_labels) + 2, column=2, padx=10, pady=10, sticky='w')
+        save_button = tk.Button(root, text="Guardar", command=self.save_to_file, font=("Berlin Sans FB", 14), bg="blue", fg="white")
+        save_button.grid(row=len(self.labels) + len(self.cost_labels) + 2, column=2, padx=10, pady=10, sticky='w')
   
        
     
